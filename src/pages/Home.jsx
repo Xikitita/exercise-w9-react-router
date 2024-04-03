3db29bf76efb3753e739105501027a2f99896da1
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-04-03 14:54:19 +0200

    Group session

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
 // TODO: Import necessary modules and data
-function Home() {
+import { songs } from "../data/songs.json";
+import { Link } from "react-router-dom";
+
+const Home = () => {
   // TODO: Implement logic to display the list of songs
+
+  const showSongs = songs.map((song) => (
+    <p key={song.rank}>
+      <Link
+        to={`/song/${song.title
+          .replace(/ /g, "-")
+          .replace(/["(", ")", "'", "."]/g, "")
+          .toLowerCase()}`}>
+        {song.title}
+      </Link>
+    </p>
+  ));
+
   return (
     <>
       <div>
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
         {/* TODO: Display the list of songs with links */}
+        <div>{showSongs}</div>
       </div>
     </>
   );
-}
+};
 
 export default Home;
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,14 +1,36 @@
 // TODO: Import necessary modules and data
+import { useParams } from "react-router-dom";
+import songsData from "../data/songs.json";
 
 // TODO: Destrcutrue props to receive information coming in from the parent comp
 function SongInfo() {
   // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+
+  const { songTitle } = useParams();
+
+  const songInfo = songsData.songs.find(
+    (song) =>
+      song.title
+        .toLowerCase()
+        .replace(/ /g, "-")
+        .replace(/["(", ")", "'", "."]/g, "") === songTitle
+  );
+
+  if (!songInfo) return <p>Song not found!</p>;
 
   return (
     <>
       <div>
         <h2>Song Information</h2>
         {/* TODO: Display song details or a "Song not found!" message */}
+        <h5>Title</h5>
+        <p>{songInfo.title}</p>
+        <h5>Artist</h5>
+        <p>{songInfo.artist}</p>
+        <h5>Year</h5>
+        <p>{songInfo.year}</p>
+        <h5>Album Name</h5>
+        <p>{songInfo.album}</p>
       </div>
     </>
   );
